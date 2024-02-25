7d4c64d8259e6654172c7b1aaab90a849a1173d4
noahbei <nbeidelman9@gmail.com>
2024-02-25 01:08:37 -0800

    add test input on start screen

--- app/index.js
+++ app/index.js
@@ -1,19 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Pressable} from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable, TextInput} from 'react-native';
 import { Link } from 'expo-router';
 import NeonButton from './components/NeonButton';
+import { useState } from 'react';
+import { ButtonGroup } from '@rneui/themed';
 
 export default function App() {
+    const [selectedIndex, setSelectedIndex] = useState([0, 2]);
+    const [text, setText] = useState('');
+
+    const handleInputChange = (input) => {
+        setText(input);
+      };
+    
+      const handleButtonPress = () => {
+        alert(`You entered: ${text}`);
+      };
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <NeonButton onPressFunction={() => {console.log("button test")}} text={'Hello'}></NeonButton>
+      {/* <NeonButton onPressFunction={() => {console.log("button test")}} text={'Hello'}></NeonButton> */}
+      
+      <Text style={styles.label}>Enter some text:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={handleInputChange}
+        value={text}
+        placeholder="Enter game key"
+      />
+      <Button title="Submit" onPress={handleButtonPress} />
+
+
+      <ButtonGroup
+        buttons={['INDOORS', 'OUTDOORS']}
+        selectedIndex={selectedIndex}
+        onPress={(value) => {
+          setSelectedIndex(value);
+        }}
+        containerStyle={{ marginBottom: 20 }}
+      />
+
       <Link href="/readyScreen" asChild>
         <Button
             title="Start Game!"
             color="#f194ff"
         />
       </Link>
+      
+      
+
     </View>
   );
 }
